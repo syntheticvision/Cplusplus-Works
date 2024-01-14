ebda1d3479e120f8dd0ca680266508b597931a07
Babak <bghafourigivi@myseneca.ca>
2024-01-14 18:59:29 -0500

    updated

--- Workshop 1/io.cpp
+++ Workshop 1/io.cpp
@@ -17,93 +17,96 @@ Date: 14 Jan 2023
 
 #include <iostream>
 #include "io.h"
-
+#include "graph.h"
 using namespace std;
 
-void printInt(int value, int fieldWidth)
+namespace seneca
 {
-    cout << value;
-    for (int i = 0; i < fieldWidth - intDigits(value); i++)
+    void printInt(int value, int fieldWidth)
     {
-        cout << " ";
-    }
-}
-int intDigits(int value)
-{
-    int count = (value == 0);
-    while (value != 0)
-    {
-        value /= 10;
-        ++count;
-    }
-    return count;
-}
-
-// Performs a fool-proof integer entry
-int getInt(int min, int max)
-{
-    int val = min - 1;
-    bool done = false;
-    while (!done)
-    {
-        cin >> val;
-        if (val < min || val > max)
+        cout << value;
+        for (int i = 0; i < fieldWidth - intDigits(value); i++)
         {
-            cout << "Invalid value!" << endl
-                 << "The value must be between " << min << " and " << max << ": ";
-        }
-        else
-        {
-            done = true;
+            cout << " ";
         }
     }
-    return val;
-}
-
-// moves the cursor backwards
-void goBack(int n)
-{
-    for (int i = 0; i < n; cout << "\b", i++)
-        ;
-}
-
-// draw line
-void labelLine(int n, const char *label)
-{
-    cout << "+";
-    for (int i = 0; i < n - 2; cout << "-", i++)
-        ;
-    cout << "+";
-    if (label)
+    int intDigits(int value)
     {
-        goBack(n - 4);
-        cout << label;
+        int count = (value == 0);
+        while (value != 0)
+        {
+            value /= 10;
+            ++count;
+        }
+        return count;
     }
-    cout << endl;
-}
-void line(int n)
-{
-    cout << "+";
-    for (int i = 0; i < n - 2; cout << "-", i++)
-        ;
-    cout << "+";
-    cout << endl;
-}
 
-// displays the user interface menu
-int menu(int noOfSamples)
-{
-    line(28);
-    cout << "| No Of Samples: ";
-    printInt(noOfSamples, 5);
-    cout << "     |" << endl;
-    line(28);
-    cout << "| 1- Set Number of Samples |" << endl;
-    cout << "| 2- Enter Samples         |" << endl;
-    cout << "| 3- Graphs                |" << endl;
-    cout << "| 0- Exit                  |" << endl;
-    cout << "\\ >                        /";
-    goBack(24);
+    // Performs a fool-proof integer entry
+    int getInt(int min, int max)
+    {
+        int val = min - 1;
+        bool done = false;
+        while (!done)
+        {
+            cin >> val;
+            if (val < min || val > max)
+            {
+                cout << "Invalid value!" << endl
+                     << "The value must be between " << min << " and " << max << ": ";
+            }
+            else
+            {
+                done = true;
+            }
+        }
+        return val;
+    }
 
-    return getInt(0, 3);
+    // moves the cursor backwards
+    void goBack(int n)
+    {
+        for (int i = 0; i < n; cout << "\b", i++)
+            ;
+    }
+
+    // draw line
+    void labelLine(int n, const char *label)
+    {
+        cout << "+";
+        for (int i = 0; i < n - 2; cout << "-", i++)
+            ;
+        cout << "+";
+        if (label)
+        {
+            goBack(n - 4);
+            cout << label;
+        }
+        cout << endl;
+    }
+    void line(int n)
+    {
+        cout << "+";
+        for (int i = 0; i < n - 2; cout << "-", i++)
+            ;
+        cout << "+";
+        cout << endl;
+    }
+
+    // displays the user interface menu
+    int menu(int noOfSamples)
+    {
+        line(28);
+        cout << "| No Of Samples: ";
+        printInt(noOfSamples, 5);
+        cout << "     |" << endl;
+        line(28);
+        cout << "| 1- Set Number of Samples |" << endl;
+        cout << "| 2- Enter Samples         |" << endl;
+        cout << "| 3- Graphs                |" << endl;
+        cout << "| 0- Exit                  |" << endl;
+        cout << "\\ >                        /";
+        goBack(24);
+
+        return getInt(0, 3);
+    }
 }

54f5faf464130362605908371a07da7f40393080
Babak <bghafourigivi@myseneca.ca>
2024-01-25 22:28:14 -0500

    updated

--- Workshop 1/DIY/graph.cpp
+++ Workshop 1/DIY/graph.cpp
@@ -22,57 +22,15 @@ using namespace std;
 
 // Maximum number of samples in an graph
 const int GRAPH_WIDTH = 65;
+const int MAX_NO_OF_SAMPLES = 20;
 
-namespace seneca
-{
-    // prints a graph comparing the sample values visually
-    void printGraph(int samples[], int noOfSamples, const char *label)
-    {
-        int max = findMax(samples, noOfSamples);
-        labelLine(GRAPH_WIDTH + 10, label);
-        for (int i = 0; i < noOfSamples; i++)
-        {
-            printBar(samples[i], max);
-        }
-        line(GRAPH_WIDTH + 10);
-    }
-
-    // prints a scaled bar relevant to the maximum value in samples array
-    void printBar(int val, int max)
-    {
-        int i;
-        int barlength = GRAPH_WIDTH * val / max;
-        cout << "| ";
-        for (i = 0; i < barlength; i++)
-        {
-            cout << "*";
-        }
-        cout << " ";
-        printInt(val, (GRAPH_WIDTH + 6 - barlength));
-        cout << "|" << endl;
-    }
-
-    // finds the largest sample in the samples array, if it is larger than GRAPH_WIDTH,
-    // otherwise it will return GRAPH_WIDTH.
-    int findMax(int samples[], int noOfSamples)
-    {
-        int max = samples[0];
-        int i;
-        for (i = 1; i < noOfSamples; i++)
-        {
-            if (max < samples[i])
-                max = samples[i];
-        }
-        return max < GRAPH_WIDTH ? GRAPH_WIDTH : max;
-    }
+namespace seneca {
 
     // Fills the samples array with the statistic samples
-    void getSamples(int samples[], int noOfSamples)
-    {
+    void getSamples(int samples[], int noOfSamples) {
         int i;
-        for (i = 0; i < noOfSamples; i++)
-        {
-            line(28);
+        for (i = 0; i < noOfSamples; i++) {
+            line(38);
             cout << "\\ " << (i + 1) << "/";
             printInt(noOfSamples, 2);
             cout << ":                    /";
@@ -80,4 +38,46 @@ namespace seneca
             samples[i] = getInt(1, 1000000);
         }
     }
+
+    // finds the largest sample in the samples array, if it is larger than GRAPH_WIDTH,
+    // otherwise it will return GRAPH_WIDTH.
+    int findMax(int samples[], int noOfSamples) {
+        int max = samples[0];
+        int i;
+        for (i = 1; i < noOfSamples; i++) {
+            if (max < samples[i]) max = samples[i];
+        }
+        return max < GRAPH_WIDTH ? GRAPH_WIDTH : max;
+    }
+
+    // prints a scaled bar relevant to the maximum value in samples array
+    void printBar(int val, int max, int count) {
+        int i;
+        int barlength = GRAPH_WIDTH * val / max;
+        if (count == 100) {
+            cout << count << " | ";
+        }
+        else {
+            cout << count << "  | ";
+        }
+        for (i = 0; i < barlength; i++) {
+            cout << "*";
+        }
+        cout << " ";
+        printInt(val, (GRAPH_WIDTH + 6 - barlength));
+        cout << "|" << endl;
+    }
+
+    // prints a graph comparing the sample values visually
+    void printGraph(int samples[], int noOfSamples, const char* label) {
+        int max = findMax(samples, noOfSamples);
+        labelLine(GRAPH_WIDTH + 10, label);
+        int count = 100;
+        for (int i = 0; i < noOfSamples; i++) {
+            printBar(samples[i], max, count);
+            count = count - 10;
+        }
+        line(GRAPH_WIDTH + 10);
+    }
+
 }
